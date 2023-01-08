cb72e47290283419c21d95b4d8695f7594db9de2
amandaroselyn <71723613+amandaroselynf@users.noreply.github.com>
2023-01-08 14:40:23 +0700

    Checkout Update

--- screens/CheckoutScreen.js
+++ screens/CheckoutScreen.js
@@ -8,16 +8,78 @@ import { firebase} from './config'
 
 function CheckoutScreen({ route, navigate }) {
      const { cart } = route.params
+     const [ subtotal, setSubtotal] = useState(0)
+     const service = useState(2)
+     const [ delivery, setDelivery] = useState(0)
+     const [ total, setTotal] = useState(0)
+
      const userId = firebase.auth().currentUser.uid
      const ordersRef = firebase.firestore().collection('orders')
      
-	const handleCheckout = async () => {
-	    
-
+	useEffect(() => {
+	   const subtotal = 0.0
+	   for(item in cart) {
+		subtotal += (item.price * item.qty)
+	    } 
+	   setDelivery(generateFees(2, 10)) 
+	   setTotal(subtotal + service + delivery) 
+	}, [] );
+	
+	const generateFees = (min, max) => {
+	    const randomNumber = Math.floor(Math.random() * max) + min
+	    return randomNumber;
 	} 
+
+	const handleCheckout = async () => {
+	   const orders = [] 
+	   await for(item in cart) {
+		const { id, name, image, price, qty } = item
+		const order = ({ 
+		  id: cartRef.doc().id,
+       		  userId: userId,
+       		  productId: product.id,
+      		  qty: qty,
+		  price: price});
+		cartRef.add(order) 
+     		.then(() => {
+		 orders.push(order) 
+       		  // alert('The product has been added to your cart!');
+     		})
+   	     }
+           navigation.navigate('OrderDetail', {
+		order: orders
+}) 
+	}
+	   
+	    
+  return (
+    <View style={styles.container}>
+        <Text style={styles.subtotalLabel}>Subtotal</Text>
+	<Text style={styles.subtotalText}>${subtotal}</Text>
+	<Text style={styles.deliveryLabel}>Delivery Fee</Text>
+	<Text style={styles.deliveryText}>${delivery}</Text>
+	<Text style={styles.serviceLabel}>Service Fee</Text>
+	<Text style={styles.serviceText}>${service}</Text>
+	<Text style={styles.totalLabel}>Total Price</Text>
+	<Text style={styles.totalText}>${total}</Text>
+	<TouchableOpacity style={styles.button} onPress={handleCheckout}>
+             <Text style={styles.purchaseText}>Purchase</Text>
+        </TouchableOpacity>
+	
+</View>
+
+
+
+) ;
+	
 
 }
 
-
+export styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        backgroundColor: '#FAFAFA',
+      },
+});
 
 export default CheckoutScreen;
