f169d4e3cd201020b5ce37b6ed172ceb7e01ffcb
amandaroselyn <71723613+amandaroselynf@users.noreply.github.com>
2023-01-08 11:45:32 +0700

    Checkout

---
+++ screens/CheckoutScreen.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState} from 'react'
+
+import { firebase} from './config'
+
+function CheckoutScreen({ route, navigate }) {
+     const { cart } = route.params
+     
+     
+	const handleCheckout = async () => {
+	    
+	} 
+
+}
+
+export default CheckoutScreen;
