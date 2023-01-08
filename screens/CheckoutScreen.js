0aabb2b6a37ef0edc13155ce187a2847816fa568
amandaroselyn <71723613+amandaroselynf@users.noreply.github.com>
2023-01-08 11:52:50 +0700

    Checkout

--- screens/CheckoutScreen.js
+++ screens/CheckoutScreen.js
@@ -1,15 +1,23 @@
-import React, { useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
+import { Image, Text, TextInput, Button, FlatList, TouchableOpacity, Pressable, View, StyleSheet, LogBox } from 'react-native'
+// import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { firebase } from '../config'
+import { Ionicons } from '@expo/vector-icons';
 
 import { firebase} from './config'
 
 function CheckoutScreen({ route, navigate }) {
      const { cart } = route.params
-     
+     const userId = firebase.auth().currentUser.uid
+     const ordersRef = firebase.firestore().collection('orders')
      
 	const handleCheckout = async () => {
 	    
+
 	} 
 
 }
+
+
 
 export default CheckoutScreen;
